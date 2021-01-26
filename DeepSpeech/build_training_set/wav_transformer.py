c28ac76c82ecb41b7734298097341522da5c205a
fhalamos <felipealamos@gmail.com>
2021-01-26 17:38:57 -0300

    Closing wav chunks after creating, avoids memory leak

--- DeepSpeech/build_training_set/wav_transformer.py
+++ DeepSpeech/build_training_set/wav_transformer.py
@@ -1,9 +1,5 @@
 from pydub import AudioSegment
 
-audio_url = "X:\\Box\\CP_Projects\\IPA_COL_Projects\\3_Ongoing Projects\\IPA_COL_COVID-19_Survey\\07_Questionnaires & Data\\04 November\\06 rawdata\\SurveyCTO\\media\\AA_9ded1778-0639-4e1b-8051-e4ce3bb3a94e_cons2_audio.m4a"
-
-audio_path = '/mnt/x/Box Sync/CP_Projects/IPA_COL_Projects/3_Ongoing Projects/IPA_COL_COVID-19_Survey/07_Questionnaires & Data/04 November/06 rawdata/SurveyCTO/media/AA_029afeb8-cf16-468f-9d86-2f79ad8978ee_cons2_audio.m4a'
-
 def transform_to_wav(audio_url, output_file_name, offset=None, duration=None):
 
     #Read file
@@ -14,13 +10,12 @@ def transform_to_wav(audio_url, output_file_name, offset=None, duration=None):
       sound = sound[offset*1000:(offset+duration)*1000]##pydub works in milliseconds
 
     #Transform to .wav
-    sound.export(output_file_name, format="wav")
+    out = sound.export(output_file_name, format="wav")
+    out.close()
 
+if __name__=='__main__':
+    audio_url = "X:\\Box\\CP_Projects\\IPA_COL_Projects\\3_Ongoing Projects\\IPA_COL_COVID-19_Survey\\07_Questionnaires & Data\\04 November\\06 rawdata\\SurveyCTO\\media\\AA_9ded1778-0639-4e1b-8051-e4ce3bb3a94e_cons2_audio.m4a"
 
-transform_to_wav(audio_path, 'wav_copy.wav')
-# i=8
-# create_wav(audio_url, output_file_name = 'primeros_'+str(i)+'.wav', offset=0, duration=i)
+    audio_path = '/mnt/x/Box Sync/CP_Projects/IPA_COL_Projects/3_Ongoing Projects/IPA_COL_COVID-19_Survey/07_Questionnaires & Data/04 November/06 rawdata/SurveyCTO/media/AA_029afeb8-cf16-468f-9d86-2f79ad8978ee_cons2_audio.m4a'
 
-
-
-# X:\Box\CP_Projects\IPA_COL_Projects\3_Ongoing Projects\IPA_COL_COVID-19_Survey\07_Questionnaires & Data\04 November\06 rawdata\SurveyCTO\media\AA_9ded1778-0639-4e1b-8051-e4ce3bb3a94e_cons2_audio.m4a
+    transform_to_wav(audio_path, 'wav_copy.wav')
