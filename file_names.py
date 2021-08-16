7eb69471009130b6bfc11f3ec1878ec15fefc23b
fhalamos <felipealamos@gmail.com>
2021-08-16 16:37:21 -0700

    Adding references to dbs files

---
+++ file_names.py
@@ -0,0 +1,7 @@
+import os
+
+TRANSCRIPTS_CACHE_FILE_NAME = os.path.join('Databases', 'transcripts_cache.json')
+TRANSCRIPT_TASKS_DB_FILE_NAME = os.path.join('Databases', 'transcript_tasks_db.json')
+QUESTION_ANALYSIS_DB_FILE_NAME = os.path.join('Databases', 'question_analysis_db.json')
+
+DS_TRAINING_TRANSCRIPTS_DB_FILE_NAME = '/mnt/c/Users/felip/ml_for_survey_data_quality/Databases/deepspeech_training_db.json'
